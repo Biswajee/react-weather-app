3d1f19d3e9f60a715d43487797aeaa1e1bcb626c
Biswajee <roy.biswajeet161@gmail.com>
2020-10-18 01:40:20 +0530

    Add state variables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import Weather from './app_component/weather.component'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'weather-icons/css/weather-icons.min.css'
+import 'weather-icons/css/weather-icons.min.css'
+import data from './secrets/secrets.json'
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${data.API_KEY}`)
+          .then(res => res.json())
+          .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    items: result
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    render(){
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                    <div className="App">
+                    <Weather />
+                    <p>items : {items} </p>
+                    </div>
+                );
+        }
+    }
 }
 
 export default App;
--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-
+import logo from '../logo.svg'
 const Weather = () => {
     return(
-    <div class="container">
-        <div className="cards">
-            <h1>Seoul</h1>
-            <h5 className="py-4">
-                <i className="wi wi-day-sunny"></i>
-            </h5>
+        <div className="card" style={{width: "18rem"}}>
+            <img className="card-img-top" src={logo} alt="Weather status"></img>
+            <div className="card-body">
+                <p className="card-text">
+                        Country : Seoul <br />
+                        Average Temperature: 25&deg; <br />
+                        Range: {minMaxTemp(22, 35)} <br />
+                        Description: Slow rain
+                </p>
+            </div>
         </div>
-    </div>
     )
 };
+
+function minMaxTemp(min, max) {
+    return(
+        <span>
+            <span className="pr-4">{min}&deg; (Min)</span>
+            <span className="pr-4">{max}&deg;(Max)</span>
+        </span>
+    )
+};
+
 
 export default Weather;
